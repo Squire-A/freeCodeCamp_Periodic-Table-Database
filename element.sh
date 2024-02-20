77e8b1acb4e4e9ebbe5f491c105600dee06be91e
Anthony Squire <squire.anthony@gmail.com>
2024-02-20 12:31:10 +0000

    fix: corrected output when no argument given

--- element.sh
+++ element.sh
@@ -6,25 +6,25 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-o
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
-fi
-
-if [[ $1 =~ ^[0-9]+$ ]]
-then
-  # lookup element with atomic number and get required info for output
-  ELEMENT_DATA=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements JOIN properties USING (atomic_number) JOIN types USING (type_id) WHERE atomic_number = $1")
-elif [[ $1 =~ ^[A-Z][a-z]?$ ]]
-then
-  # lookup element nased on symbol
-  ELEMENT_DATA=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements JOIN properties USING (atomic_number) JOIN types USING (type_id) WHERE symbol = '$1'")
 else
-  # lookup using name
-  ELEMENT_DATA=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements JOIN properties USING (atomic_number) JOIN types USING (type_id) WHERE name ILIKE '$1'")
-fi
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    # lookup element with atomic number and get required info for output
+    ELEMENT_DATA=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements JOIN properties USING (atomic_number) JOIN types USING (type_id) WHERE atomic_number = $1")
+  elif [[ $1 =~ ^[A-Z][a-z]?$ ]]
+  then
+    # lookup element nased on symbol
+    ELEMENT_DATA=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements JOIN properties USING (atomic_number) JOIN types USING (type_id) WHERE symbol = '$1'")
+  else
+    # lookup using name
+    ELEMENT_DATA=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements JOIN properties USING (atomic_number) JOIN types USING (type_id) WHERE name ILIKE '$1'")
+  fi
 
-if [[ -z $ELEMENT_DATA ]]
-then
-  echo "I could not find that element in the database."
-else
-  IFS="|" read -r ATOMIC_NUMBER SYMBOL NAME ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE <<< $ELEMENT_DATA
-  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  if [[ -z $ELEMENT_DATA ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    IFS="|" read -r ATOMIC_NUMBER SYMBOL NAME ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE <<< $ELEMENT_DATA
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  fi
 fi
