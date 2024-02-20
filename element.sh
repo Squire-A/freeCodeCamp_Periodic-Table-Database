d1f94fe2212ef3771dbb6cef41d94e193ea545a8
Anthony Squire <squire.anthony@gmail.com>
2024-02-20 11:31:35 +0000

    feat: add PSQL variable to connect to periodic table database

--- element.sh
+++ element.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 # A program to provide details on a provided element
 
+PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
